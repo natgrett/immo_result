06283fdecb7e1327047780cae22fa1c32afb473c
Nataliia Pidhorodetska <natpid1989@gmail.com>
2020-04-16 20:22:49 +0200

    Three screens

--- lib/immoObjects/immoObjects.dart
+++ lib/immoObjects/immoObjects.dart
@@ -1,1 +1,107 @@
 import 'package:flutter/material.dart';
+
+class ImmoObjects extends StatefulWidget {
+  ImmoObjects({Key key}) : super(key: key);
+
+  @override
+  _ImmoObjectsState createState() => _ImmoObjectsState();
+}
+
+class _ImmoObjectsState extends State<ImmoObjects> {
+  var productList = [
+    /*{
+      "image" : "assets/objects/1.png",
+      //first line
+      "price" : "680,000 €",
+      "quantity" : "6,325 €/m ",
+      //second line
+      "netYeld" : "2.2%",
+      "rentTrend" : "4.1%",
+      "priceTrend" : "9.6%",
+      //right side
+      "value" : "etwas teuer",
+      //not represented
+      "expRent" : "1,950 €",
+      "rentFactor" : "35",
+      "name" : "1"
+    },
+
+     */
+    {"name": "965,000 €", "image": "assets/objects/2.png", "value": "teuer"},
+    {"name": "345,788 €", "image": "assets/objects/3.png", "value": "teuer"},
+    {"name": "324,086 €", "image": "assets/objects/4.png", "value": "teuer"},
+    {"name": "399,449 €", "image": "assets/objects/5.png", "value": "teuer"},
+    {"name": "498,990 €", "image": "assets/objects/6.png", "value": "teuer"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: productList.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        childAspectRatio: MediaQuery.of(context).size.width /
+            (MediaQuery.of(context).size.height / 2.5),
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return SingleObject(
+          prodName: productList[index]['name'],
+          prodValue: productList[index]['value'],
+          prodImage: productList[index]['image'],
+        );
+      },
+    );
+  }
+}
+
+class SingleObject extends StatelessWidget {
+  final prodName;
+  final prodImage;
+  final prodValue;
+
+  SingleObject({this.prodImage, this.prodName, this.prodValue});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Hero(
+        tag: prodName,
+        child: Material(
+          child: InkWell(
+              onTap: () {},
+              child: GridTile(
+                footer: Container(
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.white,
+                    child: ListTile(
+                      //Card https://flutter.dev/docs/development/ui/layout#gridview
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
+                      dense: true,
+                      leading: new Text(
+                        prodName,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      title: Text(
+                        prodValue,
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0),
+                      ),
+
+                      subtitle: Text('3,642 €/m²'),
+                    )),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/details');
+                  },
+                  child: Image.asset(prodImage, fit: BoxFit.fitWidth),
+                ),
+              )),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:immo_result/immoObjects/immoObjects.dart';
 
 void main() => runApp(MaterialApp(
-  theme: ThemeData(fontFamily: 'MakeItSans'),
-  initialRoute: '/search',
-  routes: {
-    '/search' : (context) => SearchScreen(),
-    '/results' : (context) => ResultsScreen(),
-  },
-));
+      theme: ThemeData(fontFamily: 'MakeItSans'),
+      initialRoute: '/search',
+      routes: {
+        '/search': (context) => SearchScreen(),
+        '/results': (context) => ResultsScreen(),
+        '/details': (context) => DetailsScreen(),
+      },
+    ));
 
+//1. First Screen - Pilot Object
 class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ImmoScout24', style: TextStyle(fontFamily: 'MakeItSans', color: Colors.black),),
+        title: Text(
+          'ImmoScout24',
+          style: TextStyle(fontFamily: 'MakeItSans', color: Colors.black),
+        ),
         backgroundColor: Color(0xff66ffe3),
       ),
       body: Center(
@@ -23,12 +29,15 @@ class SearchScreen extends StatelessWidget {
           onTap: () {
             Navigator.pushNamed(context, '/results');
           },
-          child: Container (
-            child: Icon(Icons.search, size: 60.0, color: Colors.white,),
+          child: Container(
+            child: Icon(
+              Icons.search,
+              size: 60.0,
+              color: Colors.white,
+            ),
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Color(0xff00ffd0),
-
             ),
             width: 100.0,
             height: 100.0,
@@ -39,8 +48,7 @@ class SearchScreen extends StatelessWidget {
   }
 }
 
-
-
+//2. Second Screen - Results List
 class ResultsScreen extends StatefulWidget {
   ResultsScreen({Key key}) : super(key: key);
 
@@ -50,41 +58,120 @@ class ResultsScreen extends StatefulWidget {
 
 class _ResultsScreenState extends State<ResultsScreen> {
   @override
+  //World Map - Pilot Object
   Widget worldMap = new Container(
     height: 175.0,
     color: Colors.green,
-
   );
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffd6d6d6),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(65.0),
+        child: AppBar(
+          elevation: 0.0,
+          bottomOpacity: 10.0,
+          backgroundColor: Color(0xff33ffd9),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Color(0xff333333)),
+            onPressed: () => Navigator.pop(context),
+          ),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert, color: Colors.black),
+            )
+          ],
+
+          //title: Text('Results', style: TextStyle(fontFamily: 'MakeItSans', color: Colors.black),
+          title: Container(
+              margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 5.0),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(120, 255, 255, 255),
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Adjust Search",
+                          hintStyle: TextStyle(color: Color(0xff333333)),
+                          //icon: Icon(Icons.search, color: Color(0xff333333)),
+                        ),
+                      )),
+                  /*Expanded(
+                  flex: 0,
+                  child: Row(
+                    children: <Widget>[
+                      */ /*IconButton(
+                        onPressed: (){},
+                        icon: Icon(Icons.mic, color: Color(0xff333333)),
+                      ),*/ /*
+                      VerticalDivider(color: Color(0xff333333)),
+                    ],
+                  )
+                )*/
+                ],
+              )),
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.white,
+        backgroundColor: Color(0xff00ffd0),
+        buttonBackgroundColor: Color(0xff00ffd0),
+        //Color(0xff00ffd0)
+        height: 70,
+        items: <Widget>[
+          Icon(Icons.search, size: 20, color: Colors.black),
+          Icon(Icons.favorite, size: 20, color: Colors.black),
+          Icon(Icons.list, size: 20, color: Colors.black),
+          Icon(Icons.account_box, size: 20, color: Colors.black),
+        ],
+        animationDuration: Duration(milliseconds: 200),
+        animationCurve: Curves.bounceInOut,
+        onTap: (index) {
+          debugPrint("Current Index is $index");
+        },
+      ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 5.0,
+          ),
+          worldMap,
+          Container(
+            height: (MediaQuery.of(context).size.height / 1.7),
+            width: MediaQuery.of(context).size.width,
+            child: ImmoObjects(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//3.Third Screen (Details) - Pilot Object
+class DetailsScreen extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0.0,
-        bottomOpacity: 0.0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Color(0xff333333)),
-          onPressed: () => Navigator.pop(context),
+        title: Text(
+          'Details',
+          style: TextStyle(fontFamily: 'MakeItSans', color: Colors.black),
         ),
-        title: Text('Results', style: TextStyle(fontFamily: 'MakeItSans', color: Colors.black),),
+        backgroundColor: Color(0xff66ffe3),
       ),
-
-      body: ListView(
-        children: <Widget>[
-          SizedBox(height: 5.0,),
-
-          worldMap,
-
-          Container(
-            height: 400,
-            //child: ImmoObjects(),
-          )
-
-
-        ],
+      body: Center(
+        child: Image(
+            image: AssetImage('assets/objects/5.png'), fit: BoxFit.fitWidth),
       ),
-
-
     );
   }
-
 }
